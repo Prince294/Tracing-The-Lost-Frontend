1c0dac3666e2a19c6aab39873c860ff306b5f8ee
prince.kumar <prince.kumar@netprophetsglobal.com>
2023-03-22 17:59:32 +0530

    minor bug in raise exception

--- Components/History.js
+++ Components/History.js
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import http from "./Services/utility";
+import { apisPath } from "../Utils/path";
 
 export default function History() {
-    return (
-        <View>
-            <Text>History</Text>
-        </View>
-    )
+  useEffect(() => {
+    http
+      .post(apisPath?.user?.validateUsername, { username: val })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <View>
+      <Text>History</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
