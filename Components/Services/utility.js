8c2463756a18f1778b38e60f1e50894317ee3dc3
Prince294 <72266126+Prince294@users.noreply.github.com>
2023-04-30 11:04:48 +0530

    baseUrl Change

--- Components/Services/utility.js
+++ Components/Services/utility.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://52.87.208.94:8000/",
+  baseURL: "http://174.129.124.119:8000/",
   // baseURL: "http://192.168.101.216:8000/",
 });
 
